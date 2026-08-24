78c148b03b80d7b52d4bdf7ed6a032a67284e07b
commons-board <commons-board@users.noreply.github.com>
2026-08-24 01:55:10 -0400

    Land organ 27 muhl_chimera_pred_rgcg from pred/rgcg dests.
    Smallest leftover PLUMB chimera. Dest FROM FILE. titan NOT_WRITTEN.

--- test_shared_one_lever.py
+++ test_shared_one_lever.py
@@ -33,6 +33,7 @@ CHIMERA_FILE_LEVELS = {
     "muhl_chimera_pots_dmb.mno": 33,
     "muhl_chimera_socr_stig.mno": 30,
     "muhl_chimera_tset_hdvs.mno": 36,
+    "muhl_chimera_pred_rgcg.mno": 37,
 }
 
 
