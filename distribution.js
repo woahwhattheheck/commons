9f7df5e9f2691e725a1c1861db67fa489f9c8d7a
woahwhattheheck <brycembusiness2@gmail.com>
2026-08-30 01:13:27 -0400

    Fix distribution browser package UI

--- distribution.js
+++ distribution.js
@@ -1,7 +1,7 @@
 (function () {
   function esc(s) {
     return String(s == null ? "" : s).replace(/[&<>"']/g, function (c) {
-      return ({ "&": "&", "<": "<", ">": ">", '"': """, "'": "&#39;" })[c];
+      return ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" })[c];
     });
   }
   var bust = "?v=" + Date.now();
