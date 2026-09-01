80ac34f20b56885d2245971a22c88786001fcc4a
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-01 05:59:58 -0400

    ChartTrace Lane B: peer version stamps

---
+++ charttrace/peers/versions.py
@@ -0,0 +1,8 @@
+"""Version stamps for ChartTrace Lane B peer swarm."""
+
+SCHEMA_VERSION = "charttrace.peers.v1.1"
+POLICY_VERSION = "charttrace.peers.policy.v1.1"
+MODEL_VERSION = "none-local-deterministic-v1"
+PROMPT_VERSION = "charttrace.prompts.v1.1"
+GROUNDING_VERSION = "charttrace.grounding.v1.1"
+PEER_SWARM_VERSION = "charttrace.peers.swarm.v1.1"
