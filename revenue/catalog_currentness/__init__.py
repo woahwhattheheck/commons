0af8d15a6c4d8919997c51f28149a3cf60e96d80
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 06:31:22 -0400

    revenue: restore catalog currentness branch consistency

--- revenue/catalog_currentness/__init__.py
+++ revenue/catalog_currentness/__init__.py
@@ -5,11 +5,10 @@ from .audit import (
     compile_currentness,
     render_csv,
     render_markdown,
-    strict_json_loads,
     verify_receipt,
 )
 
 __all__ = [
     "CatalogCurrentnessError", "INPUT_SCHEMA", "RECEIPT_SCHEMA", "compile_currentness",
-    "render_csv", "render_markdown", "strict_json_loads", "verify_receipt",
+    "render_csv", "render_markdown", "verify_receipt",
 ]
