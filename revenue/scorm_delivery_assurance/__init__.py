51ac4b26346643cd1938e2cbeed584fdcf00ff6b
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 10:30:01 -0400

    build: expose SCORM assurance API

---
+++ revenue/scorm_delivery_assurance/__init__.py
@@ -0,0 +1,23 @@
+from .assurance import (
+    AssuranceError,
+    HOLD,
+    READY,
+    SCHEMA,
+    compile_assurance,
+    default_policy,
+    strict_json_loads,
+    validate_policy,
+    verify_assurance,
+)
+
+__all__ = [
+    "AssuranceError",
+    "HOLD",
+    "READY",
+    "SCHEMA",
+    "compile_assurance",
+    "default_policy",
+    "strict_json_loads",
+    "validate_policy",
+    "verify_assurance",
+]
