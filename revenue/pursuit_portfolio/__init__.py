9def4fbc7c2baaec6151981c73cad8c491a900e6
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 10:00:14 -0400

    pursuit portfolio: initialize package

---
+++ revenue/pursuit_portfolio/__init__.py
@@ -0,0 +1,4 @@
+"""Evidence-bound pursuit portfolio allocation."""
+from .core import PortfolioError, compile_portfolio, verify_compiled, write_compiled
+
+__all__ = ["PortfolioError", "compile_portfolio", "verify_compiled", "write_compiled"]
