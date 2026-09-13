38134be9b7622d34d25d34b7a7fb83a350a2f662
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 05:39:13 -0400

    feat(change-order): add package exports

---
+++ revenue/service_change_order_desk/__init__.py
@@ -0,0 +1,23 @@
+from .desk import (
+    DeskError,
+    append_event,
+    build_package,
+    canonical_json,
+    format_money,
+    sha256_json,
+    strict_json_loads,
+    validate_events,
+    verify_package,
+)
+
+__all__ = [
+    "DeskError",
+    "append_event",
+    "build_package",
+    "canonical_json",
+    "format_money",
+    "sha256_json",
+    "strict_json_loads",
+    "validate_events",
+    "verify_package",
+]
