df02c0d5a962e9503b6e64fd4b93edf0fd61542a
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 21:35:28 -0400

    feat(revenue): allocate accepted delivery capacity

---
+++ revenue/delivery_capacity_allocator/__init__.py
@@ -0,0 +1,3 @@
+from .engine import CapacityError, canonical_json, compile_bytes, sha256_hex, verify_current_bytes
+
+__all__ = ["CapacityError", "canonical_json", "compile_bytes", "sha256_hex", "verify_current_bytes"]
