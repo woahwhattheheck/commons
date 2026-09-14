8fc9d6c7486075ebb203822a58257af5af6be6c1
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-13 23:43:35 -0400

    revenue: add atomic organization lease package

---
+++ revenue/organization_outbound_lease/__init__.py
@@ -0,0 +1,33 @@
+"""Atomic organization-wide outbound lease.
+
+Internal coordination only. This package never authorizes or performs an external send.
+"""
+
+from .core import (
+    READY_STATE,
+    TERMINAL_STATES,
+    acquire_lease,
+    canonical_json,
+    finalize_lease,
+    fingerprint_organization,
+    mint_pressure_attestation_for_host,
+    verify_lease_document,
+    verify_pressure_attestation,
+)
+from .stores import FileLeaseStore, GitHubContentsLeaseStore, StoreConflict, StoreUncertain
+
+__all__ = [
+    "READY_STATE",
+    "TERMINAL_STATES",
+    "FileLeaseStore",
+    "GitHubContentsLeaseStore",
+    "StoreConflict",
+    "StoreUncertain",
+    "acquire_lease",
+    "canonical_json",
+    "finalize_lease",
+    "fingerprint_organization",
+    "mint_pressure_attestation_for_host",
+    "verify_lease_document",
+    "verify_pressure_attestation",
+]
