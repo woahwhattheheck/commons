3e4d95f5c62415330774bfd20e0d227eb5512fd8
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-14 19:42:17 -0400

    fix: export authority-hardened canonical lock

--- revenue/prospect_contact_lock/__init__.py
+++ revenue/prospect_contact_lock/__init__.py
@@ -8,7 +8,6 @@ from .lock import (
     CANONICAL_REPOSITORY,
     ConflictError,
     LockError,
-    ProspectContactLock,
     Receipt,
     RemoteError,
     Target,
@@ -18,6 +17,7 @@ from .lock import (
     normalize_target,
     verify_receipt,
 )
+from .hardened import ProspectContactLock
 __all__ = [
     "AUTHORITY_BRANCH", "AUTHORITY_DIGEST", "AUTHORITY_GENERATION",
     "AUTHORITY_ROOT", "CANONICAL_API_ORIGIN", "CANONICAL_REPOSITORY",
