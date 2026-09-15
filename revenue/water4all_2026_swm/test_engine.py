b2cecc84f93b20b4bc9305a247ccf915d36a0e29
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-15 01:05:45 -0400

    Water4All: include RED recovery hostiles in aggregate suite

--- revenue/water4all_2026_swm/test_engine.py
+++ revenue/water4all_2026_swm/test_engine.py
@@ -5,6 +5,7 @@ import unittest
 from .test_ready_sources import ReadyPathTests, SourceAuthorityTests
 from .test_consortium import ConsortiumTests
 from .test_evidence_cli import ApplicantAndEvidenceTests, ParsingAndCliTests
+from .test_red_recovery import RedRecoveryTests
 
 __all__ = [
     "ReadyPathTests",
@@ -12,6 +13,7 @@ __all__ = [
     "ConsortiumTests",
     "ApplicantAndEvidenceTests",
     "ParsingAndCliTests",
+    "RedRecoveryTests",
 ]
 
 if __name__ == "__main__":
