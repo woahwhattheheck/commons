d81dddea30c5ed87e176ee139d6ff97d8c8ea94d
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-15 02:02:12 -0400

    Load custody migration hostile tests

--- revenue/hive/commercial-waste-route-operations/test_desk.py
+++ revenue/hive/commercial-waste-route-operations/test_desk.py
@@ -5,6 +5,7 @@ import unittest
 from test_support import MANIFEST
 from test_desk_core import CoreDeskTests
 from test_desk_billing import BillingDeskTests
+from test_desk_migration import MigrationAuthTests
 
 
 if __name__ == "__main__":
