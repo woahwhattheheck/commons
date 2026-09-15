badd45f1c575893d5572c1ad9733f5ce9098f3a7
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-14 22:59:16 -0400

    Export NUMIH readiness compiler API

---
+++ revenue/numih_ai_sad_admission_readiness/__init__.py
@@ -0,0 +1,2 @@
+from .compiler import SCHEMA, ValidationError, compile_packet, loads_strict, render_markdown, verify_result
+__all__ = ["SCHEMA","ValidationError","compile_packet","loads_strict","render_markdown","verify_result"]
