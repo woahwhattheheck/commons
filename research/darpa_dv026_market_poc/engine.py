be479c4d8a92f4d0ef757ece274aeb60eadeaf40
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 09:17:20 -0400

    feat(dv026): export external-action evaluation API

--- research/darpa_dv026_market_poc/engine.py
+++ research/darpa_dv026_market_poc/engine.py
@@ -6,6 +6,10 @@ try:
         ContractError, MECHANISMS, STATUS_HOLD, STATUS_READY, Trader,
         canonical_bytes, loads_strict, validate_blackbox_action, validate_scenario,
     )
+    from .external_actions import (
+        ACTION_BUNDLE_SCHEMA, EXTERNAL_HOLD, EXTERNAL_READY,
+        evaluate_external_actions, render_external_markdown, verify_external_result,
+    )
     from .market import run_continuous
     from .report import render_markdown, verify_result
 except ImportError:
@@ -14,11 +18,17 @@ except ImportError:
         ContractError, MECHANISMS, STATUS_HOLD, STATUS_READY, Trader,
         canonical_bytes, loads_strict, validate_blackbox_action, validate_scenario,
     )
+    from external_actions import (
+        ACTION_BUNDLE_SCHEMA, EXTERNAL_HOLD, EXTERNAL_READY,
+        evaluate_external_actions, render_external_markdown, verify_external_result,
+    )
     from market import run_continuous
     from report import render_markdown, verify_result
 
 __all__ = [
-    "ContractError", "MECHANISMS", "STATUS_HOLD", "STATUS_READY", "Trader",
-    "canonical_bytes", "compile_result", "loads_strict", "render_markdown",
-    "run_continuous", "validate_blackbox_action", "validate_scenario", "verify_result",
+    "ACTION_BUNDLE_SCHEMA", "ContractError", "EXTERNAL_HOLD", "EXTERNAL_READY",
+    "MECHANISMS", "STATUS_HOLD", "STATUS_READY", "Trader", "canonical_bytes",
+    "compile_result", "evaluate_external_actions", "loads_strict", "render_external_markdown",
+    "render_markdown", "run_continuous", "validate_blackbox_action", "validate_scenario",
+    "verify_external_result", "verify_result",
 ]
