b4c48b706fd10dd343960fb01f4f7159bab1b6bf
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 10:29:09 -0400

    land bank v2: add non-authorizing package surface

---
+++ revenue/land_bank_online_library/__init__.py
@@ -0,0 +1,12 @@
+"""Land Bank T12-09-26 non-authorizing teaming/evidence carrier."""
+
+from .core import EvidenceRef, PaidWorkshare, PrimeCandidate, SourceCustody, compile_readiness, render_markdown
+
+__all__ = [
+    "EvidenceRef",
+    "PaidWorkshare",
+    "PrimeCandidate",
+    "SourceCustody",
+    "compile_readiness",
+    "render_markdown",
+]
