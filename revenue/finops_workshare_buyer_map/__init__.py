1cfdb20d590c7e191baa03d4f268cefd90c5de0a
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 19:05:36 -0400

    Add FinOps buyer-map package surface

---
+++ revenue/finops_workshare_buyer_map/__init__.py
@@ -0,0 +1,3 @@
+"""Evidence-bound FinOps workshare buyer map."""
+from .engine import MapError, compile_receipt, normalize_map, verify_receipt
+__all__=["MapError","compile_receipt","normalize_map","verify_receipt"]
