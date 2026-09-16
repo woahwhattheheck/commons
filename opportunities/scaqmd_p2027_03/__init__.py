98f8f4beb8051e24394c2365230083ddc16aefff
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 09:05:15 -0400

    feat(scaqmd): add pursuit package

---
+++ opportunities/scaqmd_p2027_03/__init__.py
@@ -0,0 +1,3 @@
+"""South Coast AQMD P2027-03 pursuit recovery carrier."""
+from .engine import build_source_authority, compile_assessment, verify_assessment
+__all__=["build_source_authority","compile_assessment","verify_assessment"]
