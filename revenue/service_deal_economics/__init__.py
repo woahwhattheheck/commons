4d5c2db4217b66e90e31c2dc10e7ecfdce07a2a6
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 09:23:32 -0400

    recover reviewed Service Deal Economics authority layer on current main

--- revenue/service_deal_economics/__init__.py
+++ revenue/service_deal_economics/__init__.py
@@ -1,21 +1,31 @@
+from .authority import (
+    AuthorityError,
+    authority_subject,
+    compile_current,
+    render_current_markdown,
+    verify_current_authority,
+)
 from .engine import (
     DealEconomicsError,
     canonical_json,
-    compile_report,
+    compile_report as compile_arithmetic_report,
     digest,
     parse_strict_json,
-    render_markdown,
-    verify_current,
-    verify_historical,
+    render_markdown as render_arithmetic_markdown,
+    verify_historical as verify_arithmetic_historical,
 )
 
 __all__ = [
+    "AuthorityError",
     "DealEconomicsError",
+    "authority_subject",
     "canonical_json",
-    "compile_report",
+    "compile_arithmetic_report",
+    "compile_current",
     "digest",
     "parse_strict_json",
-    "render_markdown",
-    "verify_current",
-    "verify_historical",
+    "render_arithmetic_markdown",
+    "render_current_markdown",
+    "verify_arithmetic_historical",
+    "verify_current_authority",
 ]
