eb4df8bd965783bc7cbfff6ae061271e3741be36
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 09:26:02 -0400

    Run Water4All predecessor-killer regressions

--- revenue/water4all_2026_swm/test_engine.py
+++ revenue/water4all_2026_swm/test_engine.py
@@ -5,6 +5,7 @@ import unittest
 from .test_ready_sources import ReadyPathTests, SourceAuthorityTests
 from .test_consortium import ConsortiumTests
 from .test_evidence_cli import ApplicantAndEvidenceTests, ParsingAndCliTests
+from .test_security_regressions import AuthorityBoundaryRegressionTests, ConsortiumAndPartnerRegressionTests
 
 __all__ = [
     "ReadyPathTests",
@@ -12,6 +13,8 @@ __all__ = [
     "ConsortiumTests",
     "ApplicantAndEvidenceTests",
     "ParsingAndCliTests",
+    "AuthorityBoundaryRegressionTests",
+    "ConsortiumAndPartnerRegressionTests",
 ]
 
 if __name__ == "__main__":
