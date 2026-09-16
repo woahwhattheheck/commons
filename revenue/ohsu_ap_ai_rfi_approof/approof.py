50bb48c21d13ccd2c3e9ab08bc0ff6f418e74729
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-16 08:28:17 -0400

    fix(revenue): route APProof public API through authority binding

--- revenue/ohsu_ap_ai_rfi_approof/approof.py
+++ revenue/ohsu_ap_ai_rfi_approof/approof.py
@@ -1,6 +1,7 @@
 """Stable public APProof API."""
-from .common import APProofError, AUTHORITY, canonical_json_bytes
-from .engine import compile_packet, verify_projection
+from .common import APProofError, canonical_json_bytes
+from .authority_api import AUTHORITY, compile_packet, verify_projection
+
 __all__ = [
     "APProofError", "AUTHORITY", "canonical_json_bytes",
     "compile_packet", "verify_projection",
