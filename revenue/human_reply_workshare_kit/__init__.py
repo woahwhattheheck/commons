df6576ef67b271e7708bac055aa4d40498478fe9
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-17 02:48:09 -0400

    TYPE: export workshare compiler API

---
+++ revenue/human_reply_workshare_kit/__init__.py
@@ -0,0 +1,23 @@
+from .core import (
+    COMMERCIAL_STATE,
+    SCHEMA,
+    CompiledOffer,
+    WorkshareError,
+    compile_offer,
+    pack_catalog,
+    render_offer_markdown,
+    render_receipt_json,
+    strict_json_loads,
+)
+
+__all__ = [
+    "COMMERCIAL_STATE",
+    "SCHEMA",
+    "CompiledOffer",
+    "WorkshareError",
+    "compile_offer",
+    "pack_catalog",
+    "render_offer_markdown",
+    "render_receipt_json",
+    "strict_json_loads",
+]
