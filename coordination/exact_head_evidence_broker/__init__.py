eee545c66554dbfef6728f91e0857010a099a7c9
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-17 17:23:19 -0400

    Coordination: add exact-head broker package surface

---
+++ coordination/exact_head_evidence_broker/__init__.py
@@ -0,0 +1,2 @@
+from .core import BrokerError, compile_current, loads_strict_json, verify_current, verify_integrity
+__all__=['BrokerError','compile_current','loads_strict_json','verify_current','verify_integrity']
