ba225d4ac27d0217cf86bef4a2621ece1d693f65
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-17 23:20:43 -0400

    workfeed planner: make test carrier substantive

--- tests/test_wfpp_extra.py
+++ tests/test_wfpp_extra.py
@@ -1,5 +1,17 @@
 import unittest
 
-class ConnectorProbe(unittest.TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+from tools.workfeed_poll_planner.core import PlannerError, strict_json_loads
+
+
+class ParserTests(unittest.TestCase):
+    def test_duplicate_key_is_rejected(self):
+        with self.assertRaises(PlannerError):
+            strict_json_loads('{"a":1,"a":2}')
+
+    def test_float_is_rejected(self):
+        with self.assertRaises(PlannerError):
+            strict_json_loads('{"a":1.5}')
+
+
+if __name__ == "__main__":
+    unittest.main()
