6c1634e7ac21bc9a07a537184b70f8692afdb17c
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-18 03:43:55 -0400

    Build revenue collection control plane

---
+++ tools/revenue_collection_desk/__init__.py
@@ -0,0 +1,27 @@
+from .core import (
+    ACTIONS,
+    AUTHORITY,
+    ContractError,
+    LEDGER_SCHEMA,
+    REPORT_SCHEMA,
+    canonical_bytes,
+    compile_json,
+    compile_ledger,
+    loads_strict,
+    verify_json,
+    verify_ledger,
+)
+
+__all__ = [
+    "ACTIONS",
+    "AUTHORITY",
+    "ContractError",
+    "LEDGER_SCHEMA",
+    "REPORT_SCHEMA",
+    "canonical_bytes",
+    "compile_json",
+    "compile_ledger",
+    "loads_strict",
+    "verify_json",
+    "verify_ledger",
+]
