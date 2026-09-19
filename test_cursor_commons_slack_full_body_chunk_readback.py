ffa6d6e86b4f0b3276458f5f0aa5c4aa6595e79f
woahwhattheheck <brycembusiness2@gmail.com>
2026-09-19 19:20:26 -0400

    test: repin current full-body chunk catalog

--- test_cursor_commons_slack_full_body_chunk_readback.py
+++ test_cursor_commons_slack_full_body_chunk_readback.py
@@ -18,7 +18,7 @@ DOOR = ROOT / "commons-slack-chunk.html"
 KEEP = {
     "p/cursor-commons-slack-full-body-chunk-20260902-01.md": "94770f41",
     "host/commons_slack_full_body_chunk.py": "b132f8bb",
-    "ground/COMMONS_SLACK_FULL_BODY_CHUNK.json": "3dc4d270",
+    "ground/COMMONS_SLACK_FULL_BODY_CHUNK.json": "df1d7af1",
     "test_commons_slack_full_body_chunk.py": "25174d80",
     "commons-slack-chunk.html": "4d9ebea2",
     "host/commons_slack_full_body.py": "7a6067d7",
